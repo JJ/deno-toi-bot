e6f310fc8530f92f313a11b3fc8f6ddd677f93f3
JJ Merelo <jjmerelo@gmail.com>
2023-09-11 13:51:18 +0200

    Saving to a file, closes #2

--- main.ts
+++ main.ts
@@ -7,15 +7,26 @@ const TOKEN = Deno.env.get("TOI_BOT_TOKEN");
 if (!TOKEN) throw new Error("Bot token is not provided");
 const bot = new TelegramBot(TOKEN);
 
+const prefijo = Deno.args[0] || "asistencia";
+
 const date = new Date().toISOString().slice(0, 10);
-const asistenciaFile = `asistencia-${date}.json`;
-const file = await Deno.open(asistenciaFile, { create: true, write: true });
+const asistenciaFile = `${prefijo}-${date}.txt`;
 
 bot.on(UpdateType.Message, async ({ message }) => {
   console.log(message);
   const nick: string = message.from?.username?.toLowerCase();
+  const checkInFile = await Deno.open(asistenciaFile, {
+    create: true,
+    write: true,
+    append: true,
+  });
   const mensaje: string = `👋 ${escapeLodash(nick)} 📅 ${escapeDash(date)}`;
   console.warn(mensaje);
+  // append at the end of checkInFile the nick
+  await Deno.writeAll(checkInFile, new TextEncoder().encode(`${nick}\n`));
+
+  checkInFile.close();
+
   await bot.sendMessage({
     chat_id: message.chat.id,
     parse_mode: "MarkdownV2",
