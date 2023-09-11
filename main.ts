8d624e2d4c25f128a7820fad2ec81f790efc48b3
JJ Merelo <jjmerelo@gmail.com>
2023-09-11 09:59:30 +0200

    :construction: #1

--- main.ts
+++ main.ts
@@ -3,30 +3,18 @@ import {
   UpdateType,
 } from "https://deno.land/x/telegram_bot_api/mod.ts";
 
-import { Notas } from "./src/notas.ts";
-
-const TOKEN = Deno.env.get("DENOTASBOT_TOKEN");
+const TOKEN = Deno.env.get("TOI_BOT_TOKEN");
 if (!TOKEN) throw new Error("Bot token is not provided");
 const bot = new TelegramBot(TOKEN);
-const notas = new Notas(Deno.args[0] || "data/notas-iv-22-23-ordinaria.csv");
-await notas.setup();
+
+const date = new Date().toISOString().slice(0, 10);
+const asistenciaFile = `asistencia-${date}.json`;
+const file = await Deno.open(asistenciaFile, { create: true, write: true });
+
 bot.on(UpdateType.Message, async ({ message }) => {
   console.log(message);
   const nick: string = message.from?.username?.toLowerCase();
-  const estasNotas = notas.notas.get(nick);
-  let mensaje: string;
-  if (estasNotas === undefined) {
-    mensaje =
-      `⚠️ Parece que no he encontrado tu nick _${nick}_ en la lista\n` +
-      "¿Es posible que te dieras de alta con otro?";
-  } else {
-    mensaje =
-      `🧑‍🏫 Nota de *${escapeLodash(nick)}*\n` +
-      `❧Proyecto: _${escapeDot(estasNotas?.notaProyecto)}_\n` +
-      `❧Extra: _${escapeDot(estasNotas?.notaExtra)}_\n` +
-      `❧Presentación: _${escapeDot(estasNotas?.notaPresentacion)}` +
-      `_\n❧Final: *_||${escapeDot(estasNotas?.notaFinal)}||_*`;
-  }
+  let mensaje: string = `👋*${escapeLodash(nick)} 📅 ${date}`;
   console.warn(mensaje);
   await bot.sendMessage({
     chat_id: message.chat.id,
@@ -38,10 +26,6 @@ bot.on(UpdateType.Message, async ({ message }) => {
 bot.run({
   polling: true,
 });
-
-function escapeDot(nota) {
-  return (nota + "").replace(".", "'");
-}
 
 function escapeLodash(nick) {
   return nick.replace("_", "\\_");
